acbbe311e218d05407a5400c8c0b7ec396f8fb08
jradisson@seven-shapes.com <thespacemustache@gmail.com>
2020-01-13 14:20:53 +0100

    Added possibility to change framerate

---
+++ TestSocio/Assets/IndieMarc/PlatformerDemo/Scripts/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public int targetFrame = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.targetFrameRate != targetFrame)
+            Application.targetFrameRate = targetFrame;
+    }
+}
